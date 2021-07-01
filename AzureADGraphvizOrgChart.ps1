4a395046f51196344cc0b246e7ae980e37120eab
rbarbrow <rbarbrow@markonsolutions.com>
2021-06-30 23:19:34 -0400

    Reversed hierarchy and added native graphviz svg creation

--- AzureADGraphvizOrgChart.ps1
+++ AzureADGraphvizOrgChart.ps1
@@ -1,8 +1,20 @@
 
 #set path of saved .dot file
-$path = "C:\Users\rbarbrow\OneDrive - Markon Solutions\Desktop\"
+$path = "C:\Users\rbarbrow\OneDrive - Markon Solutions\Desktop"
+$dotfile ="\orgchart.dot"
+$orgfile = "\orgchart.svg"
+
+
+$DOTpath =$path+$dotfile
+$ORGpath =$path+$orgfile
+
 #array of names to ignore
 $ignore = @("Blue Desk", "Bot", "Canary Bot", "Help", "Help Con", "Help Fin", "Help HR", "Help IT", "Help Marketing", "Help Office Admin", "Help Rec", "Help Sec", "Help Solutions", "HelpProp", "HQ Innovation Lab", "HQ Training Room", "HQ Well Room", "innovationlab", "Peerless Admin", "Red Desk", "Yellow Desk")
+#path for graphviz dot.exe file
+$graphvizPath = "C:\Program Files\Graphviz\bin\dot.exe"
+
+
+
 
 #connect to azure AD (will prompt you sometimes it hides the window behind other things)
 Connect-AzureAD
@@ -30,9 +42,9 @@ foreach ($user in $users) {
         #checks if the manager is null also replaces any spaces in the name
         if($null -eq $manager.DisplayName) 
         {
-            $sb.AppendLine( $user.DisplayName.replace(" ","_")+ " ->  None" )
+            $sb.AppendLine(  "None -> " + $user.DisplayName.replace(" ","_") )
         }else {
-            $sb.AppendLine( $user.DisplayName.replace(" ","_")+ " -> "+ $manager.DisplayName.replace(" ","_"))
+            $sb.AppendLine( $manager.DisplayName.replace(" ","_")+ " -> "+ $user.DisplayName.replace(" ","_") )
         }
     }
 }
@@ -43,7 +55,10 @@ $sb.AppendLine("}")
 $sb = $sb.Replace(".","")
 $sb = $sb.Replace("'","")
 
-$sb.ToString() | Out-File $path\orgchart.dot
+$sb.ToString() | Out-File $DOTpath
+
 
 #will add code to run graphviz dot.exe natively here
-#test test test test 
+#dot -Tpng input.dot > output.png
+#dot -Tps filename.dot -o outfile.ps
+cmd.exe /c C:\Program Files\Graphviz\bin\dot.exe -Tsvg $DOTpath -o $ORGpath
